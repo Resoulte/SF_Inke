c5c9532c182ce52cb5197d9d1dad167894bfcef5
Resolute <sferic@163.com>
2016-09-06 16:09:36 +0800

    完善观看功能

--- SF_Inke/SF_Inke/Classes/Show/Controller/SFLiveChatViewController.m
+++ SF_Inke/SF_Inke/Classes/Show/Controller/SFLiveChatViewController.m
@@ -13,9 +13,16 @@
 @property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
 @property (weak, nonatomic) IBOutlet UILabel *peoplecountLable;
 
+@property (weak, nonatomic) IBOutlet UIButton *shareBtn;
+/**计时器*/
+@property (strong, nonatomic) dispatch_source_t timer;
 @end
 
 @implementation SFLiveChatViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self initTimer];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -27,14 +34,74 @@
     [NSTimer scheduledTimerWithTimeInterval:0.25 block:^(NSTimer * _Nonnull timer) {
         self.peoplecountLable.text = [NSString stringWithFormat:@"%u人", arc4random_uniform(10000)];
     } repeats:YES];
+    
+    [self initTimer];
 
+}
 
+// 初始化心形动画
+- (void)initTimer {
+    
+    //初始化心形动画
+    self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(self.timer, DISPATCH_TIME_NOW, 0.25 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(self.timer, ^{
+        [self showMoreLoveAnimateFromView:self.shareBtn addToView:self.view];
+    });
+    dispatch_resume(self.timer);
+
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+
+    [self showMoreLoveAnimateFromView:self.shareBtn addToView:self.view];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)showMoreLoveAnimateFromView:(UIView *)fromView addToView:(UIView *)addToView {
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 30, 25)];
+    CGRect loveFrame = [fromView convertRect:fromView.frame toView:addToView];
+    CGPoint position = CGPointMake(fromView.layer.position.x, loveFrame.origin.y - 30);
+    imageView.layer.position = position;
+    NSArray *imgArr = @[@"heart_1",@"heart_2",@"heart_3",@"heart_4",@"heart_5",@"heart_1"];
+    NSInteger img = arc4random()%6;
+    imageView.image = [UIImage imageNamed:imgArr[img]];
+    [addToView addSubview:imageView];
+    
+    imageView.transform = CGAffineTransformMakeScale(0.01, 0.01);
+    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0.5 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        imageView.transform = CGAffineTransformIdentity;
+    } completion:nil];
+    
+    CGFloat duration = 3 + arc4random()%5;
+    CAKeyframeAnimation *positionAnimate = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    positionAnimate.repeatCount = 1;
+    positionAnimate.duration = duration;
+    positionAnimate.fillMode = kCAFillModeForwards;
+    positionAnimate.removedOnCompletion = NO;
+    
+    UIBezierPath *sPath = [UIBezierPath bezierPath];
+    [sPath moveToPoint:position];
+    CGFloat sign = arc4random()%2 == 1 ? 1 : -1;
+    CGFloat controlPointValue = (arc4random()%50 + arc4random()%100) * sign;
+    [sPath addCurveToPoint:CGPointMake(position.x, position.y - 300) controlPoint1:CGPointMake(position.x - controlPointValue, position.y - 150) controlPoint2:CGPointMake(position.x + controlPointValue, position.y - 150)];
+    positionAnimate.path = sPath.CGPath;
+    [imageView.layer addAnimation:positionAnimate forKey:@"heartAnimated"];
+    
+    
+    [UIView animateWithDuration:duration animations:^{
+        imageView.layer.opacity = 0;
+    } completion:^(BOOL finished) {
+        [imageView removeFromSuperview];
+    }];
+}
+
 
 #pragma mark - setter and getter
 - (void)setLiveItem:(SFLiveItem *)liveItem {
